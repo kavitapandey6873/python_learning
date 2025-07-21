8453a4e7e7bcc29ee6e165a93a287b3246f5a586
Kavita Pandey <kavita617435@gmail.com>
2025-07-21 17:35:08 +0530

    Add files via upload

---
+++ my_list3.py
@@ -0,0 +1,7 @@
+#count method in python
+list2=[10,3.5,'shivani','aditya',10]
+print(list2.count(10))
+
+#index method in python
+listp=[8,9.2,'anu','shreya','anu','False']
+print(listp.index('anu'))
