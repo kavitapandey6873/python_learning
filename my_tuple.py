8980cbb41c6a6f9c5cee07ed2955668f14c5e665
Kavita Pandey <kavita617435@gmail.com>
2025-07-22 23:30:34 +0530

    Add files via upload

---
+++ my_tuple.py
@@ -0,0 +1,19 @@
+#empty tuple in python
+var=()
+print(type(var))
+
+#tuple with some values in python
+T=(10,'sneha','False',7.5)
+print(T)
+
+a=(6,'ishu',2.5,True)
+print(len(a))
+
+p=(2,'yukti',5.3,'False')
+tuple1=p
+print(tuple1)
+
+print(p[3])
+
+c=p.index("False")
+print(c)
